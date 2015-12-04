b8086e56ef217a0d15f5b7daa76e448def8c420c
Tyler Hines <tylerhines1992@gmail.com>
2015-12-04 14:01:35 -0500

    Add put request

--- server.js
+++ server.js
@@ -60,7 +60,35 @@ app.delete('/todos/:id', function (req, res) {
 
 		res.json(matchedTodo);
 	}
+})
 
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+	var body = _.pick(req.body, 'description', 'completed'); // Use _.pick to only pick description and completed
+	var validAttributes = {};
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (!matchedTodo) {
+		return res.status(404).json({"error": "No todo found with that id."});
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	} else {
+		// Never provided attribute, no problem here
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes)
+	res.json(matchedTodo);
 })
 
 
