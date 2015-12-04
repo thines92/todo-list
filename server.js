11ee7c1dc31e9b23aea609aa1c090ed42018441d
Tyler Hines <tylerhines1992@gmail.com>
2015-12-04 12:32:07 -0500

    Add delete request

--- server.js
+++ server.js
@@ -48,6 +48,22 @@ app.post('/todos', function (req, res) {
 	res.json(todos);
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (!matchedTodo) {
+		res.status(404).json({"error": "No todo found with that id."});
+	} else {
+		todos = _.without(todos, matchedTodo);
+
+		res.json(matchedTodo);
+	}
+
+})
+
+
 app.listen(PORT, function() {
 	console.log('Express listening on port ' + PORT + '...');
 });
