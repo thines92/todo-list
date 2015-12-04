47ac0273462e31fcdbc16fe22152bdd5844b5774
Tyler Hines <tylerhines1992@gmail.com>
2015-12-04 12:07:54 -0500

    Add _.pick to server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -19,14 +20,8 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	// Iterate over todos array. Find the match.
-	todos.forEach(function (todo) {
-		if (todoId === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	// serach todo array for todoId
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -37,11 +32,17 @@ app.get('/todos/:id', function (req, res) {
 
 // POST /todos
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed'); // Use _.pick to only pick description and completed
 
-	// add id field
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0) {
+		return res.status(400).send();
+	}
+
+	// Set body.description to be trimmed value
+	body.description = body.description.trim();
+
 	body.id = todoNextId++;
-	// push body into array
+
 	todos.push(body);
 
 	res.json(todos);
