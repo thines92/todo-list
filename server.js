3691bd75743e463911716ae2919faa32a3383122
Tyler Hines <tylerhines1992@gmail.com>
2015-12-04 11:12:53 -0500

    Add post request

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Start node.js course',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -42,6 +34,18 @@ app.get('/todos/:id', function (req, res) {
 		res.status(404).send();
 	}
 })
+
+// POST /todos
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	// add id field
+	body.id = todoNextId++;
+	// push body into array
+	todos.push(body);
+
+	res.json(todos);
+});
 
 app.listen(PORT, function() {
 	console.log('Express listening on port ' + PORT + '...');
