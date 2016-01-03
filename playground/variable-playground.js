58ca4619b65c6aa9a2f28d769d2f062861dd3f74
Tyler Hines <tylerhines1992@gmail.com>
2016-01-03 12:04:35 -0500

    Add sequelize demo

---
+++ playground/variable-playground.js
@@ -0,0 +1,31 @@
+// var person = {
+// 	name: 'Tyler',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Tyler',
+// 	// 	age: 24
+// 	// }
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array example
+
+grades = [15, 37];
+
+function addGrade (gradesArr) {
+	gradesArr.push(55);
+	debugger;
+
+	// No update
+
+	// gradesArr = [12, 33, 89];
+}
+
+addGrade(grades);
+console.log(grades);
