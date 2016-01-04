f72a8ab58c39457949072f27c0de9c901b74716b
Tyler Hines <tylerhines1992@gmail.com>
2016-01-04 11:03:10 -0500

    Convert GET /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -36,40 +36,33 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
+	db.todo.findById(todoId).then(function (todo) {
+		if (!!todo) {
+			res.json(todo.toJSON())
+		} else {
+			res.status(404).send();
+		}
+	}, function (e) {
+		res.status(500).send();
+	});
+	// var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	// if (matchedTodo) {
+	// 	res.json(matchedTodo);
+	// } else {
+	// 	res.status(404).send();
+	// }
 })
 
 // POST /todos
 app.post('/todos', function (req, res) {
 	var body = _.pick(req.body, 'description', 'completed'); // Use _.pick to only pick description and completed
 
-	// call create on db.todo
-	// 	  respond with 200 and todo (to json)
-	//	  res.status(400).json(e)
-
 	db.todo.create(body).then(function (todo) {
 		res.json(todo.toJSON());
 	}, function (e) {
 		res.status(400).json(e);
 	})
-
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0) {
-	// 	return res.status(400).send();
-	// }
-
-	// body.description = body.description.trim();
-
-	// body.id = todoNextId++;
-
-	// todos.push(body);
-
-	// res.json(todos);
 });
 
 // DELETE /todos/:id
