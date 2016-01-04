6ec867178df29c07934bbb392f4f4d40a5f46bea
Tyler Hines <tylerhines1992@gmail.com>
2016-01-04 13:58:03 -0500

    Update PUT /todos/:id to use sequelize

--- db.js
+++ db.js
@@ -13,10 +13,6 @@ if (env === 'production') {
 	});
 }
 
-// var sequelize = new Sequelize(undefined, undefined, undefined, {
-// 	'dialect': 'sqlite',
-// 	'storage': __dirname + '/data/dev-todo-api.sqlite'
-// });
 var db = {};
 
 db.todo = sequelize.import(__dirname + '/models/todo.js');
