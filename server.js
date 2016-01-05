cdb6f065e4c2d09bd401a0d40deb1a131b7b6e62
Tyler Hines <tylerhines1992@gmail.com>
2016-01-05 09:49:01 -0500

    Add todo/user association

--- server.js
+++ server.js
@@ -59,7 +59,11 @@ app.post('/todos', middleware.requireAuthentication, function (req, res) {
 	var body = _.pick(req.body, 'description', 'completed'); // Use _.pick to only pick description and completed
 
 	db.todo.create(body).then(function (todo) {
-		res.json(todo.toJSON());
+		req.user.addTodo(todo).then(function () {
+			return todo.reload();
+		}).then(function (todo) {
+			res.json(todo.toJSON());
+		});
 	}, function (e) {
 		res.status(400).json(e);
 	})
