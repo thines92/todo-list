f74c2aed8b5e978fc4ef1599801e514c209b967c
Tyler Hines <tylerhines1992@gmail.com>
2016-01-05 07:15:42 -0500

    Add authenticate function

--- server.js
+++ server.js
@@ -129,27 +129,14 @@ app.post('/users', function (req, res) {
 app.post('/users/login', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
-	if (typeof body.email !== 'string' || typeof body.password !== 'string') {
-		return res.status(400).send();
-	}
-
-	db.user.findOne({
-		where: {
-			email: body.email
-		}
-	}).then(function (user) {
-		if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
-			return res.status(401).send();
-		}
-
-
-		res.status(200).json(user.toPublicJSON());
-	}, function (e) {
-		res.status(500).json(e);
+	db.user.authenticate(body).then(function (user) {
+		res.json(user.toPublicJSON());
+	}, function () {
+		res.status(401).send();
 	})
 });
 
-db.sequelize.sync().then(function () {
+db.sequelize.sync({force: true}).then(function () {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '...');
 	});
